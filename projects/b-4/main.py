a37faae62b98ab71be624577ae40883b24f66735
ramin_yz <yazdani76ramin@gmail.com>
2023-07-05 21:31:58 +0330

    Ramin Yazdani | b-4 | chore
    - edit main.py
    # create todo 
    ## func : get_name
    ## func : process_name
    ## func : process flames

---
+++ projects/b-4/main.py
@@ -0,0 +1,18 @@
+# todo define function to get a name from input
+def get_name():
+    pass
+
+
+# todo process name : function gets two names and remove duplicated letters from them and return one list
+def process_name(name1, name2):
+    pass
+
+
+# todo process flames : function gets a list of letters and return the result of flames game
+def process_flames(letters):
+    pass
+
+
+# todo main
+if __name__ == '__main__':
+    pass
