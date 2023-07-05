d700a669b9b3567c9126200e109218562dda96d7
ramin_yz <yazdani76ramin@gmail.com>
2023-07-05 21:51:03 +0330

    Ramin Yazdani | b-4 | chore
    - edit main.py
    # add dataclass FlamesData
    # minor edits in other functions

--- projects/b-4/main.py
+++ projects/b-4/main.py
@@ -1,17 +1,45 @@
+from dataclasses import dataclass, field
+
+
+
+
+@dataclass
+class FlamesData:
+    flames_result: str
+    name_1: str = None
+    name_2: str = None
+    letters: list = field(default_factory = list)
+
+    FLAMES = ("FRIENDS", "LOVERS", "AFFECTIONATE", "MARRIAGE", "ENEMIES", "SIBLINGS")
+
+    def result(self):
+        if self.flames_result:
+            return self.flames_result
+        else:
+            self.flames_result = process_flames(self.letters)
+            return self.flames_result
+
+    def can_play(self):
+        if self.name_1 and self.name_2:
+            if self.letters == []:
+                self.letters = process_name(self.name_1,
+                                            self.name_2)
+            return True
+        else:
+            return False
+
 # todo define function to get a name from input
 def get_name():
-    pass
-
+    while True:
+        temp = input("Enter your name: ")
 
 # todo process name : function gets two names and remove duplicated letters from them and return one list
-def process_name(name1, name2):
+def process_name(name1: str, name2: str) -> list:
     pass
-
 
 # todo process flames : function gets a list of letters and return the result of flames game
-def process_flames(letters):
+def process_flames(letters: list) -> str:
     pass
-
 
 # todo main
 if __name__ == '__main__':
