4ca2836ae0f7384ae3fc4be46e7844f3c0d7b668
ramin_yz <yazdani76ramin@gmail.com>
2023-07-05 22:59:12 +0330

    Ramin Yazdani | b-5 | chore
    - edit main.py
    # define class  thread : keyboard

--- projects/b-5/main.py
+++ projects/b-5/main.py
@@ -1,17 +1,39 @@
+import threading
+import time
+
+import keyboard
+
 # todo program threads
 class Threds:
-    # todo keyaboard thread
-    class Keyboard:
-        pass
+    class Keyboard(threading.Thread):
+
+        def __init__(self):
+            super(Threds.Keyboard,
+                  self).__init__()
+            self.sig = threading.Event()
+            self.data = ""
+            self.temp = ""
+
+        def run(self) -> None:
+            while self.sig.is_set() is False:
+                self.temp = keyboard.read_key()
+                if self.temp in ['r', 'p', 's', 'esc']:
+                    self.data = self.temp
+                    self.temp = ""
+                    if self.data == "esc":
+                        self.sig.set()
+                        break
+                else:
+                    time.sleep(0.1)
+            
 
     # todo output printing thread
-    class Output:
+    class Output(threading.Thread):
         pass
 
     # todo game mechanics thread
-    class Mechanics:
+    class Mechanics(threading.Thread):
         pass
-
 
 if __name__ == '__main__':
     pass
