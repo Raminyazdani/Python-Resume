fc605de0db318925754de6a0c3c6c52d06e9d6a9
ramin_yz <yazdani76ramin@gmail.com>
2023-07-05 22:52:41 +0330

    Ramin Yazdani | b-5 | chore
    - edit main.py
    # create todo 
    ## thread keyboard
    ## thread mechanics
    ## thread output

---
+++ projects/b-5/main.py
@@ -0,0 +1,17 @@
+# todo program threads
+class Threds:
+    # todo keyaboard thread
+    class Keyboard:
+        pass
+
+    # todo output printing thread
+    class Output:
+        pass
+
+    # todo game mechanics thread
+    class Mechanics:
+        pass
+
+
+if __name__ == '__main__':
+    pass
